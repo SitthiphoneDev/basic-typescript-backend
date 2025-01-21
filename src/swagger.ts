7f6d6a19de5c243eed9af65f5f287009da5cb4ab
Sitthiphone <aun@comet.la>
2025-01-22 02:13:45 +0700

    fix: config

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,20 +2,24 @@ import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
     definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Product Management API Mobile App CS',
-        version: '1.0.0',
-        description: 'API documentation for Product Management System',
-      },
-      servers: [
-        {
-          url: 'http://localhost:3000',
-          description: 'Development server',
+        openapi: '3.0.0',
+        info: {
+            title: 'Product Management API Mobile App CS',
+            version: '1.0.0',
+            description: 'API documentation for Product Management System',
         },
-      ],
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Development server',
+            },
+            {
+                url: 'https://ecommerce-app-hv1l.onrender.com',  // Your Render URL
+                description: 'Production server',
+            }
+        ],
     },
     apis: ['./src/router/*.ts'], // Path to the API routes
-  };
-  
-  export const specs = swaggerJsdoc(options);
+};
+
+export const specs = swaggerJsdoc(options);
