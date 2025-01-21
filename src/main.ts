9fdb2eb2a47fabf8a100cf6dcbbf135291629857
Sitthiphone <Sitthiphone.pv@gmail.com>
2025-01-22 03:09:07 +0700

    fix: config deploy

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,29 @@
-import express from 'express';
+import express, { Application } from 'express';
 import morgan from 'morgan';
 import categoryRouter from './router/category.routes';
 import unitRouter from './router/unit.routes';
 import productRouter from './router/product.routes';
-import { specs } from './swagger';
 import swaggerUi from 'swagger-ui-express';
+import { specs } from './swagger';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
-const host = 'localhost';
-const port = 3000;
-
-const app = express();
+const app: Application = express();
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0'; 
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
 app.use('/api/categories', categoryRouter);
 app.use('/api/units', unitRouter);
 app.use('/api/products', productRouter);
+
+// Swagger Docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.listen(port, host, () => {
+// Start the server
+app.listen(Number(port), host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
